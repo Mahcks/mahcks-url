1d73bf793f7556f36b500e135ae80d43f2069edf
Max <maxnatural13@gmail.com>
2022-01-29 16:02:05 -0600

    Added API key

--- routes/url.js
+++ routes/url.js
@@ -8,8 +8,17 @@ require('dotenv').config();
 const router = express.Router()
 
 // import the Url database model
-const Url = require('../models/Url')
-
+const Url = require('../models/Url');
+function checkForKey(req, res) {
+  const apiKey = req.get("API-Key");
+  const keys = process.env.API_KEY?.split(" ");
+  if (!apiKey || !keys?.includes(apiKey)) {
+    res.status(401).json({ error: "Unauthorized" });
+    return false;
+  } else {
+    return true;
+  }
+}
 // @route    POST /api/url/shorten
 // @description     Create short URL
 
@@ -17,54 +26,57 @@ const Url = require('../models/Url')
 const baseUrl = process.env.BASE_URL;
 
 router.post('/shorten', async (req, res) => {
-  const {
-    longUrl
-  } = req.body // destructure the longUrl from req.body.longUrl
+  let isAuth = checkForKey(req, res);
+  if (isAuth) {
+    const {
+      longUrl
+    } = req.body // destructure the longUrl from req.body.longUrl
 
-  // check base url if valid using the validUrl.isUri method
-  if (!validUrl.isUri(baseUrl)) {
-    return res.status(401).json('Invalid base URL')
-  }
+    // check base url if valid using the validUrl.isUri method
+    if (!validUrl.isUri(baseUrl)) {
+      return res.status(401).json('Invalid base URL')
+    }
 
-  // if valid, we create the url code
-  const urlCode = shortid.generate()
+    // if valid, we create the url code
+    const urlCode = shortid.generate()
 
-  // check long url if valid using the validUrl.isUri method
-  if (validUrl.isUri(longUrl)) {
-    try {
-      /* The findOne() provides a match to only the subset of the documents 
-      in the collection that match the query. In this case, before creating the short URL,
-      we check if the long URL was in the DB ,else we create it.
-      */
-      let url = await Url.findOne({
-        longUrl
-      })
-
-      // url exist and return the respose
-      if (url) {
-        res.json(url)
-      } else {
-        // join the generated short code the the base url
-        const shortUrl = baseUrl + '/' + urlCode
-
-        // invoking the Url model and saving to the DB
-        url = new Url({
-          longUrl,
-          shortUrl,
-          urlCode,
-          date: new Date()
+    // check long url if valid using the validUrl.isUri method
+    if (validUrl.isUri(longUrl)) {
+      try {
+        /* The findOne() provides a match to only the subset of the documents 
+        in the collection that match the query. In this case, before creating the short URL,
+        we check if the long URL was in the DB ,else we create it.
+        */
+        let url = await Url.findOne({
+          longUrl
         })
-        await url.save()
-        res.json(url)
+
+        // url exist and return the respose
+        if (url) {
+          res.json(url)
+        } else {
+          // join the generated short code the the base url
+          const shortUrl = baseUrl + '/' + urlCode
+
+          // invoking the Url model and saving to the DB
+          url = new Url({
+            longUrl,
+            shortUrl,
+            urlCode,
+            date: new Date()
+          })
+          await url.save()
+          res.json(url)
+        }
       }
+      // exception handler
+      catch (err) {
+        console.log(err)
+        res.status(500).json('Server Error')
+      }
+    } else {
+      res.status(401).json('Invalid longUrl')
     }
-    // exception handler
-    catch (err) {
-      console.log(err)
-      res.status(500).json('Server Error')
-    }
-  } else {
-    res.status(401).json('Invalid longUrl')
   }
 })
 
